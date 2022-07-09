db57a51eca5e399c61e977d5c53b94485bf2f59b
shubhamsigdar1 <shubhamsigdar@gmail.com>
2022-07-09 19:10:05 +0530

    adding coding challenge

--- 7th Kyu/Split In Parts.js
+++ 7th Kyu/Split In Parts.js
@@ -29,4 +29,4 @@ var splitInParts = function(s, partLength){
   
   console.log(splitInParts("supercalifragilisticexpialidocious", 3), "sup erc ali fra gil ist ice xpi ali doc iou s")
   console.log(splitInParts("HelloKata", 1), "H e l l o K a t a")
-  console.log(splitInParts("HelloKata", 9), "HelloKata")
+  console.log(splitInParts("HelloKata", 9))
