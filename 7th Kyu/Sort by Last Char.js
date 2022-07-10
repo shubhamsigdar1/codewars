b7610de833898f4baa11b93a78aa2b6b732d8fd8
shubhamsigdar1 <shubhamsigdar@gmail.com>
2022-07-10 20:56:22 +0530

    adding coding challenge

--- 7th Kyu/Sort by Last Char.js
+++ 7th Kyu/Sort by Last Char.js
@@ -6,8 +6,8 @@
 function last(x){
     return x.split(' ').sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
   }
-  // console.log(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up'])
-  // console.log(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what'])
-  // console.log(last('take me to semynak'), ['take', 'me', 'semynak', 'to'])
-  last('take me to semynak')
+  console.log(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up'])
+  console.log(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what'])
+  console.log(last('take me to semynak'), ['take', 'me', 'semynak', 'to'])
+//   last('take me to semynak')
   
